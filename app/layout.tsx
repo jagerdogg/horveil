03a71490592a5c14eda50a7fdfdb8da982060e44
Stephen Ireland <sgireland@gmail.com>
2026-03-20 14:33:47 -0400

    custom favicon, update layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
